351dbc4ca3b9f5676ab9ac2caaf85c4d8651c26f
OPmasterLEO <98689894+OPmasterLEO@users.noreply.github.com>
2026-08-04 22:44:48 +0200

    update dependencies to latest

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,18 +6,19 @@ java {
     withSourcesJar()
     withJavadocJar()
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
 
 dependencies {
     api(project(":nms-api"))
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnly("io.netty:netty-transport:4.1.115.Final")
-    compileOnly("io.netty:netty-handler:4.1.115.Final")
-    compileOnly("net.kyori:adventure-api:4.17.0")
-    compileOnly("net.kyori:adventure-text-serializer-plain:4.17.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("io.netty:netty-transport:4.2.16.Final")
+    compileOnly("io.netty:netty-handler:4.2.16.Final")
+    compileOnly("net.kyori:adventure-api:5.2.0")
+    compileOnly("net.kyori:adventure-text-serializer-plain:5.2.0")
+    testImplementation(platform("org.junit:junit-bom:6.1.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
